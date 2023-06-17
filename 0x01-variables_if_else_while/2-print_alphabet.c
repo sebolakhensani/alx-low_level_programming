faaa084565461543a2185b4b501911ae63171ada
sebolakhensani <khensanisebola51@gmail.com>
2023-06-17 11:30:46 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return(0);
+}
