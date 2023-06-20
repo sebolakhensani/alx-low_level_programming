237ef48d5f96b18e98fc55a453ff6efb8a02df04
sebolakhensani <khensanisebola51@gmail.com>
2023-06-20 12:54:39 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,15 @@
 
 /**
  * print_alphabet - Make the alphabet
- *
- * Return:void
+
+ * Return: void
  */
 
 void print_alphabet(void)
-
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 _putchar(ch);
 _putchar('\n');
+
 }
